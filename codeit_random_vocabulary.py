21684c877603187dfd1aeab886af429595f46263
minjae <32402820+kkrlee@users.noreply.github.com>
2019-02-10 16:14:37 -0500

    fixed variable 'dict' to 'vocab'

--- codeit_random_vocabulary.py
+++ codeit_random_vocabulary.py
@@ -1,20 +1,19 @@
 from random import randint
 
 in_file = open("vocabulary.txt", "r")
-dict = {}
+vocab = {}
 
 for line in in_file:
     data = line.strip().split(" : ")
     english_word = data[0]
     korean_meaning = data[1]
-    dict[korean_meaning] = english_word
-
-keys = list(dict.keys())
+    vocab[korean_meaning] = english_word
 
 while True:
+    keys = list(vocab.keys())
     random_num = randint(0, len(keys)-1)
     korean_meaning = keys[random_num]
-    english_word = dict[korean_meaning]
+    english_word = vocab[korean_meaning]
     guess = input("%s : " % korean_meaning)
     if guess == english_word:
         print("맞았습니다!")
